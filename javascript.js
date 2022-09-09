79c7c4991dcb05ae9a7750d4d43ab418150a2569
glennderry530 <glennderry@gmail.com>
2022-09-09 16:29:46 -0700

    grid aspect ratio and multiplier fixed

--- javascript.js
+++ javascript.js
@@ -1,22 +1,51 @@
 
 
 
-let grid = document.getElementById('grid');
 
-let columns = 32, rows =  32;
+function createGrid(rowtot) {
+    let gridContainer = document.querySelector('.grid');
+    
+    let celltot = rowtot * rowtot; 
 
-let cells = document.createElement('cells');
-cells.className = 'cells';
-for (let i = 0; i < columns; i++) {
-    let column = document.createElement('div');
-    column.className = ('column');
-    for (let j = 0; j < rows; j++) {
-        let row = document.createElement('div');
-        row.className = ('row');
-        row.textContent = i + '-' + j; //col - row in cell
-        column.appendChild(row); // append rows in Column
-    }
-    cells.appendChild(column); // append column in cell
+    gridContainer.style.display = 'grid';
+    gridContainer.style.gridTemplateRows = `repeat(${rowtot}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${rowtot}, 1fr)`;
+   
+    let row = 1;
+    let column = 1;
+    for (let i = 1; i <= celltot; i++) {
+        let cell = document.createElement('div');
+        cell.setAttribute("id", "cell");
+        cell.style.border = '1px solid lightgrey';
+        cell.style.gridRow = row;
+        cell.style.gridColumn = column;
+        cell.style.aspectRatio = '1 / 1';
+        cell.textContent = (null);
+        column += 1;
+        if (column === rowtot+1) {
+            row += 1;
+            column = 1;
+        }
+        gridContainer.appendChild(cell);
+    };
+        
+};
+   
+createGrid (16);
+
+
+
+/*  FIX THIS SECTION
+let color = "red"
+
+let cells = document.getElementById('cell');
+cells.addEventListener("mouseover", func, false);
+cells.addEventListener("mouseout", func1, false);
+
+function func() {
+    cells.setAttribute("style", "background-color:blue;")
 }
-
-grid.appendChild(cells);
+function func1() {
+    cells.setAttribute("style", "background-color:green;")
+}
+*/
