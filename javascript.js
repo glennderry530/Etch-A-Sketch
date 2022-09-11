4f0b7de010dfd5846b251c3f60f36f6523d04a32
glennderry530 <glennderry@gmail.com>
2022-09-11 16:12:09 -0700

    button and colorpicker logic functional

--- javascript.js
+++ javascript.js
@@ -1,48 +1,97 @@
+const DEFAULT_SIZE = 16;
+const DEFAULT_COLOR = '#222222';
+const DEFAULT_BGCOLOR = '#EEEEEE';
+const DEFAULT_TOOL = 'colorPicker'
+
+let currentTool = DEFAULT_TOOL;
+let currentColor = DEFAULT_COLOR;
+let currentGridSize = DEFAULT_SIZE;
+let currentBGColor = DEFAULT_BGCOLOR;
+
+const gridContainer = document.querySelector('.grid');
+const colorPicker = document.getElementById("colorPicker");
+const clearButton = document.getElementById("clearButton");
+const eraser = document.getElementById("eraser");
+
+const penLabel = document.getElementById("penLabel");
+const eraserLabel = document.getElementById("eraserLabel");
+const clearLabel = document.getElementById("clearLabel");
 
 
+colorPicker.onchange = function () {changePenColor(colorPicker.value)};
 
+function changePenColor (newColor) {currentColor = newColor};
 
-function createGrid(rowtot) {
-    let gridContainer = document.querySelector('.grid');
-    
-    let celltot = rowtot * rowtot; 
+eraser.onclick = function () {changePenColor('')};
 
-    gridContainer.style.display = 'grid';
-    gridContainer.style.gridTemplateRows = `repeat(${rowtot}, 1fr)`;
-    gridContainer.style.gridTemplateRows = `repeat(${rowtot}, 1fr)`;
-   
-    let row = 1;
-    let column = 1;
-    for (let i = 1; i <= celltot; i++) {
-        let cell = document.createElement('div');
-        cell.setAttribute("id", "cell");
-        cell.setAttribute("class", "cell");
-        cell.style.border = '1px solid lightgrey';
-        cell.style.gridRow = row;
-        cell.style.gridColumn = column;
-        cell.style.aspectRatio = '1 / 1';
-        cell.textContent = (null);
-        column += 1;
-        if (column === rowtot+1) {
-            row += 1;
-            column = 1;
-        }
-        gridContainer.appendChild(cell);
-    };
-    
+clearButton.onclick = function () {
+    changeGridSize(currentGridSize); changePenColor(colorPicker.value);
 };
-   
-createGrid (16);
 
+function toolSelect(newTool){
+    changeTool(newTool);
+    currentTool = newTool;
+    console.log(newTool);
+};
 
-const color = ('red'); 
-
-function changeGridColor (e) {
-    
-    e.style.backgroundColor=color;
+function changeTool(toolChoice){
+if (toolChoice = colorPicker) {
+    penLabel.style.fontWeight = 'bold';
+} else if (toolChoice = eraser) {
+    eraser.style.fontWeight = 'bold';
+} else if (toolChoice = clearButton) {
+    clearButton.style.fontWeight = 'bold';
+}
 }
 
-// "is mouse button down section" - from here https://tinyurl.com/58574ep6
+
+//Slider Section
+const slider = document.getElementById("myRange");
+const gridNum = document.getElementById("numGrids");
+gridNum.textContent = slider.value; // Display the default slider value
+
+slider.oninput = function() { //Update the gridNum text as you drag the slider handle
+  gridNum.textContent = this.value;
+};
+
+slider.onchange = function() { //change the grid size after you select size
+    changeGridSize(this.value);
+};
+
+
+
+
+function changeGridSize (total) {
+    currentGridSize = total;
+    clearGrid();
+    createGrid(total);
+};
+
+function clearGrid() {
+    document.querySelector('.grid').innerHTML=('')
+};
+
+//Create grids - from https://tinyurl.com/mrxutdkn
+function createGrid(size) {
+    let cellTotal = size * size; 
+    gridContainer.style.setProperty('--grid-rows', size);
+    gridContainer.style.setProperty('--grid-cols', size);
+    
+    for (let i = 0; i < cellTotal; i++) {
+        let cell = document.createElement('div');
+        cell.setAttribute("id", "cell");
+        cell.style.border = '1px solid darkgrey';
+        cell.style.aspectRatio = '1 / 1';
+        gridContainer.appendChild(cell).className = "cell";
+    };
+};
+
+function changeGridColor (e) {
+    e.style.backgroundColor=currentColor;
+};
+
+
+// "is mouse button down section" - https://tinyurl.com/58574ep6 (this section could definatly be better)
 let primaryMouseButtonDown = false;
 
 function setPrimaryButtonState(e) {
@@ -55,9 +104,15 @@ document.addEventListener("mousemove", setPrimaryButtonState);
 document.addEventListener("mouseup", setPrimaryButtonState);
 // "end of is mouse button down section"
 
-
 document.addEventListener('mousemove', function(e) {
     if(e.target && e.target.id== ('cell') && primaryMouseButtonDown === true){
         changeGridColor (e.target)
     }
 });
+
+
+window.onload = () => {
+    createGrid(DEFAULT_SIZE);
+    toolSelect(DEFAULT_TOOL);
+}
+
